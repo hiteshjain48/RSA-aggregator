6ea237091b9bb9a2a4e5c02a6a49f324a5280ae3
Hitesh Jain <hiteshjainhd@gmail.com>
2023-08-15 21:37:00 +0530

    sqlc

--- main.go
+++ main.go
@@ -14,12 +14,11 @@ import (
 func main(){
 	fmt.Println("Hello world!")
 	godotenv.Load(".env")
-	portString:=os.Getenv("PORT")
+	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Port is not found in the environment")
 	}
 	router := chi.NewRouter()
-
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"https://*", "http://*"},
 		AllowedMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
@@ -28,6 +27,12 @@ func main(){
 		AllowCredentials: false,
 		MaxAge: 300,
 	}))
+
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
